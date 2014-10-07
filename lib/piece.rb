e1fbafa1ffd484b76d6dbb1c1cf239ccc66658e6
Benjamin Pearce <hawk.git@bcpearce.com>
2014-10-07 16:20:53 -0400

    gave piece a color

---
+++ lib/piece.rb
@@ -0,0 +1,12 @@
+class Piece
+
+  def initialize(color = "white")
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+end
+
---
+++ spec/piece_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/piece.rb'
+
+describe Piece do 
+  
+  it { should respond_to :color }
+
+end

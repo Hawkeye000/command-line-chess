1b9981a5d6ad52fb471889db444f78f000cd940b
Benjamin Pearce <hawk.git@bcpearce.com>
2014-10-23 19:35:27 -0400

    edited board to use a to_s, added basic routes for sinatra

--- lib/board.rb
+++ lib/board.rb
@@ -8,7 +8,7 @@ class Board
     ("A".."H").each do |letter|
       ("1".."8").each do |number|
         loc = "#{letter}#{number}"
-        piece =  
+        piece =
           if number == "7"
             Pawn.new("black", loc)
           elsif loc == "A8" || loc == "H8"
@@ -17,11 +17,11 @@ class Board
             Knight.new("black", loc)
           elsif loc == "C8" || loc == "F8"
             Bishop.new("black", loc)
-          elsif loc == "D8" 
+          elsif loc == "D8"
             Queen.new("black", loc)
           elsif loc == "E8"
             King.new("black", loc)
-            
+
           elsif number == "2"
             Pawn.new("white", loc)
           elsif loc == "A1" || loc == "H1"
@@ -30,13 +30,13 @@ class Board
             Knight.new("white", loc)
           elsif loc == "C1" || loc == "F1"
             Bishop.new("white", loc)
-          elsif loc == "D1" 
+          elsif loc == "D1"
             Queen.new("white", loc)
           elsif loc == "E1"
             King.new("white", loc)
           else
             " "
-          end 
+          end
         @board[loc] = piece
       end
     end
@@ -106,7 +106,7 @@ class Board
       self.set_piece(piece)
       castle_complement(piece, self)
       return loc_2
-    else 
+    else
       return nil
     end
   end
@@ -124,10 +124,11 @@ class Board
     @board.to_a
   end
 
-  def display
-    puts "  " + ("A".."H").to_a.join(" ")
+  def to_s
+    string = ""
+    string += "  " + ("A".."H").to_a.join(" ") + "\n"
     ("1".."8").to_a.reverse.each do |row|
-      print row + " "
+      string += (row.to_s + " ")
       ("A".."H").each do |col|
         background =
           if (col.ord + row.to_i).even?
@@ -135,11 +136,15 @@ class Board
           else
             :red
           end
-        print (@board["#{col}#{row}"].to_s + " ").colorize(:background => background)
+        string += (@board["#{col}#{row}"].to_s + " ").colorize(:background => background)
         end
-      print " #{row}\n"
+      string += " #{row}\n"
     end
-    puts "  " + ("A".."H").to_a.join(" ")
+    string += ("  " + ("A".."H").to_a.join(" ") + "\n")
+  end
+
+  def display
+    print self.to_s
   end
 
   def on_board?(loc)
